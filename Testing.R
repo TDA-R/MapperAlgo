740f18e95aaaaaa7754e38ae30645fb1cd00ee8e
Wang chi-chien <kennywang2003@gmail.com>
2025-08-22 21:22:08 +0800

    optimize cover function

--- Testing.R
+++ Testing.R
@@ -37,17 +37,18 @@ time_taken <- system.time({
   Mapper <- MapperAlgo(
     # filter_values = iris[,1:4],
     filter_values = circle_data[,2:3],
-    intervals = 4,
-    percent_overlap = 30, 
+    percent_overlap = 20, 
     methods = "dbscan",
-    method_params = list(eps = 0.3, minPts = 5),
+    method_params = list(eps = 0.3, minPts = 1),
     # methods = "hierarchical",
     # method_params = list(num_bins_when_clustering = 10, method = 'ward.D2'),
     # methods = "kmeans",
     # method_params = list(max_kmeans_clusters = 2),
     # methods = "pam",
     # method_params = list(num_clusters = 2),
-    cover_type = 'extension',
+    cover_type = 'stride',
+    # intervals = 4,
+    interval_width = 1,
     num_cores = 12
     )
 })
